dea0434b44f0a48183222f2fab76febaee41ab4f
lonelyPotato <52703619+lonelyPotat0@users.noreply.github.com>
2020-08-23 11:43:11 +0700

    Update loginSubmit.php

--- loginSubmit.php
+++ loginSubmit.php
@@ -5,7 +5,7 @@ if(isset($_POST['login']))
     if (!$conn) {
         die('Could not connect my sql:' .mysql_error()); 
     }
-    $sql = `SELECT * FROM user where username=$_REQUEST['username'] and password=$_REQUEST['password'] `; 
+    $sql = `SELECT count(*) FROM user where username=$_REQUEST['username'] and password=$_REQUEST['password'] `; 
     $result = mysqli_query($conn , $sql);
     //$row = mysqli_fetch_object($result) > 0;
     $row = $result->fetch_row() > 0;
