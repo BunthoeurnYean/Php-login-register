60f9576bf9a88a15620c8becab61fa77c0b6a5a6
yim sarakoudom <yim.oudom@gmail.com>
2020-08-24 10:53:50 +0700

    fix login

--- loginSubmit.php
+++ loginSubmit.php
@@ -2,10 +2,12 @@
 if(isset($_POST['login']))
 {
     $conn = mysqli_connect('localhost', 'root', '', 'student'); 
-    if (!$conn) {
+    if (!$conn) {   
         die('Could not connect my sql:' .mysql_error()); 
     }
-    $sql = `SELECT count(*) FROM user where username=$_REQUEST['username'] and password=$_REQUEST['password'] `; 
+    $userName = $_REQUEST['username'];
+    $password = $_REQUEST['password'];
+    $sql = `SELECT count(*) FROM user where username= $userName and password=$password `; 
     $result = mysqli_query($conn , $sql);
     //$row = mysqli_fetch_object($result) > 0;
     $row = $result->fetch_row() > 0;
